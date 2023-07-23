1769f1038294a0d3cedca00675ae7f309387cc36
cjparsonson <cjparsonson@gmail.com>
2023-07-23 22:07:12 +0100

    Add project files.

---
+++ TaxCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using TaxClassLib;
+
+WriteLine("Please enter the total cost of your purchase: ");
+string? cost = ReadLine();
+
+Calculator calc = new Calculator("UK");
+calc.TotalCost = Convert.ToDecimal(cost);
+
+calc.CalculateTax();
+
+WriteLine($"The tax amount is {calc.TaxAmount:C}");
+WriteLine($"The cost minus tax is {calc.CostMinusTax:C}");
+WriteLine($"The total cost is {calc.TotalCost:C}");
+
+
+
+
+
---
+++ TaxClassLib/Calculator.cs
@@ -0,0 +1,66 @@
+﻿namespace TaxClassLib;
+
+public class Calculator
+{
+    // Fields
+    protected string? locality;
+    protected decimal taxRate;
+    protected decimal costMinusTax;
+    protected decimal taxAmount;
+    protected decimal totalCost;
+
+    // Properties
+    public string? Locality
+    {
+        get { return locality; }
+        set { locality = value; }
+    }
+
+    public decimal TaxRate
+    {
+        get { return taxRate; }
+        set { taxRate = value; }
+    }
+
+    public decimal CostMinusTax
+    {
+        get { return costMinusTax; }
+        set { costMinusTax = value; }
+    }
+
+    public decimal TaxAmount
+    {
+        get { return taxAmount; }
+        set { taxAmount = value; }
+    }
+
+    public decimal TotalCost
+    {
+        get { return totalCost; }
+        set { totalCost = value; }
+    }
+
+    // Constructors
+    public Calculator(string Locality)
+    {
+        if (Locality.ToLower() == "uk")
+        {
+            locality = "UK";
+            taxRate = 0.2m;
+        }
+
+    }
+
+    // Methods
+    private void calculateTax() // private method for encapsulation
+    {
+        taxAmount = taxRate * totalCost;
+        costMinusTax = totalCost - taxAmount;
+    }
+
+    public void CalculateTax() // exposed method for use by client code
+    {
+        calculateTax();
+    }
+
+}
